ea90c8ec4059bf04c97a1a72a7baa48c7a48df88
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-03-30 18:35:35 -0300

    Added comments

--- semaforo.ino
+++ semaforo.ino
@@ -15,16 +15,16 @@
 #define BOTTOM A1
 #define RIGHT_BOTTOM A2
 #define LEFT_BOTTOM 2
-int count = 0;
+int count = 0; // count time in seconds
 int display_count = 0;
 int pedestrian_flag = 0;
-int pedestrian_count = 0;
+int pedestrian_count = 0; // saves count value when there are pedestrians
 int toogle = 0;
-int night_flag = 0;
-unsigned int timer_to_wake = 0;
-unsigned int timer_to_sleep = 0;
+int night_flag = 0;  
+unsigned int timer_to_wake = 0; // counter to  daylight mode
+unsigned int timer_to_sleep = 0; // counter to nighttime mode
 
-void timer_display(int number)
+void timer_display(int number) // decoder for 7 segment display
 {
   switch(number)
   {
@@ -121,21 +121,21 @@ void timer_display(int number)
   }
 }
 
-void traffic_light(int color)
+void traffic_light(int color) // turn on/off LEDs like a traffic light
 {
-  if (color == 4)
+  if (color == RED_T)
   {
     digitalWrite(RED_T,HIGH);
     digitalWrite(YELLOW,LOW);
     digitalWrite(GREEN_T,LOW);
   }
-  else if (color == 5)
+  else if (color == YELLOW)
   {
     digitalWrite(YELLOW,HIGH);
     digitalWrite(RED_T,LOW);
     digitalWrite(GREEN_T,LOW);
   }
-  else if (color == 6)
+  else if (color == GREEN_T)
   {
     digitalWrite(YELLOW,LOW);
     digitalWrite(RED_T,LOW);
@@ -143,7 +143,34 @@ void traffic_light(int color)
   }
 }
 
-void ISR_button()
+void pedestrian_light(int color) // turn on/off LEDs for pedestrian light
+{
+  if (color == GREEN_P)
+  {
+    digitalWrite(RED_P,LOW);
+    digitalWrite(GREEN_P,HIGH);
+  }
+  if (color == RED_P)
+  {
+    digitalWrite(GREEN_P,LOW);
+    digitalWrite(RED_P,HIGH);
+  }
+}
+
+void toogle_led(int color)
+{
+  if(toogle == 0)
+    {
+      digitalWrite(color,LOW);
+      toogle = 1;
+    }
+  else if(toogle == 1)
+    {
+      digitalWrite(color,HIGH);
+      toogle = 0;
+    }
+}
+void ISR_button() // activate flag for pedestrian
 {
   pedestrian_flag = 1;
   pedestrian_count = count;
@@ -153,8 +180,9 @@ void ISR_button()
 
 void ISR_timer()
 {
-  
-  if (analogRead(A0) > 400)
+
+  // LDR data for daylight and nighttime modes
+  if (analogRead(A0) > 600) 
   {
     timer_to_wake = 0;
     timer_to_sleep += 1;
@@ -165,7 +193,8 @@ void ISR_timer()
     timer_to_wake += 1;
   }
 
-  if(pedestrian_flag == 0 && night_flag == 0)
+  // daylight normal mode
+  if(pedestrian_flag == 0 && night_flag == 0) 
   {
     switch(count)
     {
@@ -175,7 +204,7 @@ void ISR_timer()
         break;
       case 0 :
         traffic_light(GREEN_T);
-        digitalWrite(RED_P,HIGH);
+        pedestrian_light(RED_P);
         display_count = 9;
         break;
       case 10 :
@@ -184,46 +213,33 @@ void ISR_timer()
         break;
       case 15 :
         traffic_light(RED_T);
-        digitalWrite(RED_P,LOW);
-        digitalWrite(GREEN_P,HIGH);
+        pedestrian_light(GREEN_P);
         display_count = 24;
         break;
      } 
      timer_display(display_count - count);
   }
-  else if(pedestrian_flag == 1 && night_flag == 0)
+
+  // pedestrian mode
+  else if(pedestrian_flag == 1 && night_flag == 0) 
     {
         if(count == pedestrian_count + 5)
         {
-          digitalWrite(RED_T,LOW);
-          digitalWrite(GREEN_T,LOW);
-          digitalWrite(YELLOW,HIGH);
+          traffic_light(YELLOW);
           display_count = pedestrian_count + 9;
         }
         if(count == pedestrian_count + 10)
         {
-          digitalWrite(YELLOW,LOW);
-          digitalWrite(RED_T,HIGH);
-          digitalWrite(GREEN_P,HIGH);
-          digitalWrite(RED_P,LOW);
+          traffic_light(RED_T);
+          pedestrian_light(GREEN_P);
           display_count = pedestrian_count + 18;
         }
         if(count >= pedestrian_count + 14 && count <= pedestrian_count + 19)
         {
-          if(toogle == 0)
-          {
-            digitalWrite(GREEN_P,LOW);
-            toogle = 1;
-          }
-          else if(toogle == 1)
-          {
-            digitalWrite(GREEN_P,HIGH);
-            toogle = 0;
-          }
+          toogle_led(GREEN_P);
           if(count == pedestrian_count + 19)
           {
-            digitalWrite(GREEN_P,LOW);
-            digitalWrite(RED_P,HIGH);
+            pedestrian_light(RED_P);
             count = -1;
             pedestrian_count = 0;
             pedestrian_flag = 0;
@@ -233,25 +249,17 @@ void ISR_timer()
         {
           timer_display(display_count - count);
         }
-
     }
-    else if(night_flag == 1)
+
+    // nighttime mode
+    else if(night_flag == 1) 
     {
       pedestrian_flag = 0;
       digitalWrite(RED_T,LOW);
       digitalWrite(RED_P,LOW);
       digitalWrite(GREEN_T,LOW);
       digitalWrite(GREEN_P,LOW);
-      if(toogle == 0)
-        {
-          digitalWrite(YELLOW,LOW);
-          toogle = 1;
-        }
-        else if(toogle == 1)
-        {
-          digitalWrite(YELLOW,HIGH);
-          toogle = 0;
-        }
+      toogle_led(YELLOW);
       count = -1;
       timer_display(0);
     }
@@ -260,7 +268,7 @@ void ISR_timer()
 
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
   pinMode(3, INPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
@@ -272,16 +280,15 @@ void setup() {
   pinMode(11,OUTPUT);
   pinMode(12,OUTPUT);
   pinMode(A1,OUTPUT);
-  pinMode(2,OUTPUT);
   pinMode(A2,OUTPUT);
-  Timer1.initialize(1000000);
+  Timer1.initialize(1000000); // interrupt every 1 second
   Timer1.attachInterrupt(ISR_timer);
-  attachInterrupt(digitalPinToInterrupt(3),ISR_button,RISING);
+  attachInterrupt(digitalPinToInterrupt(3),ISR_button,RISING); // pedestrian interrupt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (timer_to_sleep > 5)
+  // conditions for daylight and nighttime mode
+  if (timer_to_sleep > 5) 
   {
     night_flag = 1;
     digitalWrite(13,HIGH);
