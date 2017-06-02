440fba326ea4cc400944be229fa06e59e2f421f2
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-06-02 18:37:42 -0300

    Works properly

--- motor.ino
+++ motor.ino
@@ -5,33 +5,36 @@
 
 #define BUFFER_SIZE 15
 
-int flag = 0;
+int flag = 0; // flag to initiate control algorithm
 int vel;
 char data[15];
 int buffer_position = 0;
 int string_complete = 0;
 int end_write = 0;
-int overflow = 0;
-int count_int = 0;
+int count = 0;
+volatile int count_int = 0;
 float vel_measure = 0;
+float error = 0;
+int pwm = 0;
+float a = 0.01;
 
 
-ISR(TIMER2_OVF_vect)
+ISR(TIMER2_OVF_vect) // timer interrupt for sampling time
 {
-	overflow++;
+	count++;
+	if (count == 3)
+	{
+		vel_measure = count_int*300;
+		count = 0;
+		count_int = 0;
+		flag = 1; 
+	}
 }
 
-void ISR_button()
+void ISR_button() // interrupt counts sensor interrupts
 {
-	flag = 1;
-	TCNT2 = 0;
+	//flag = 1;
 	count_int++;
-	if (overflow*255 + TCNT2 >= 781)
-	{
-		vel_measure = count_int/0.1;
-		overflow = 0;
-		count_int = 0;
-	}
 }
 
 
@@ -62,6 +65,7 @@ void serialEvent() // receives UART messages and saves it in the buffer
       {
         buffer_add(c);
       }
+      count = 0;
   }
 }
 
@@ -75,22 +79,21 @@ void clean_buffer()
   }
 }
 
-void timer2()
+void timer2() //timer for data aquisition
 {
-	TCCR2 |= (1<<CS22) | (1<<CS21) | (1<<CS20);
+	TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);
 	TCNT2 = 0;
-	TIMSK |= (1<<TOIE2);
+	TIMSK2 |= (1<<TOIE2);
 	sei();
 }
 void setup()
 {
-	pinMode(2,OUTPUT);// enable A
-	pinMode(3,INPUT); // interrupção
-	pinMode(4,OUTPUT); // direção
-	pinMode(5,OUTPUT); // PWM
+	pinMode(3,INPUT); // external interrupt
+	pinMode(4,OUTPUT); // IN1 pin in H-bridge for clockwise motion
+	pinMode(5,OUTPUT); // PWM, pin ENA in H-bridge
 	digitalWrite(2,HIGH);
 	digitalWrite(4,HIGH);
-	attachInterrupt(digitalPinToInterrupt(3),ISR_button,RISING);
+	attachInterrupt(digitalPinToInterrupt(3),ISR_button,RISING); 
 	Serial.begin(9600);
 	timer2();
 }
@@ -98,7 +101,7 @@ void setup()
 
 void loop()
 {
-	Serial.println(overflow);
+// -------------Receives velocity from serial-----
 	if (string_complete == 1)
 	{
 		char number[4];
@@ -112,19 +115,20 @@ void loop()
         number[i] = data[i];
         
       }
-       vel = atoi(number);
+       vel = atoi(number); //vel
        clean_buffer();
 	}
-
-	if (vel != 0)
-	{
-		//Serial.println(vel);
-	}
-
-	analogWrite(5, vel);
+//-------------proportional control algorithm-----------------------
 	if (flag == 1)
 	{
-		Serial.println("Interrompeu");
+		Serial.println(vel_measure);
+		error = vel_measure - vel;
+		pwm = pwm - a*error;
+		if (pwm >= 255)
+		{
+			pwm = 255;
+		}
+		analogWrite(5, pwm);
 		flag = 0;
 	}
 }
